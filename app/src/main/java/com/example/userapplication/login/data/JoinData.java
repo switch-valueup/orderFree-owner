6fc40266d6428fca2e58c5d37baf322af5d417bc
Yeon <jryoun1@naver.com>
2020-06-21 12:17:18 +0900

    회원가입 항목(userID, userMobilePhone)추가

--- app/src/main/java/com/example/userapplication/login/data/JoinData.java
+++ app/src/main/java/com/example/userapplication/login/data/JoinData.java
@@ -3,18 +3,28 @@ package com.example.userapplication.login.data;
 import com.google.gson.annotations.SerializedName;
 
 public class JoinData {
-    @SerializedName("userName")
-    private String userName;
-
-    @SerializedName("userEmail")
-    private String userEmail;
+    @SerializedName("userID")
+    private String userID;
 
     @SerializedName("userPwd")
     private String userPwd;
 
-    public JoinData(String userName, String userEmail, String userPwd) {
-        this.userName = userName;
-        this.userEmail = userEmail;
+    @SerializedName("userEmail")
+    private String userEmail;
+
+    @SerializedName("userName")
+    private String userName;
+
+    @SerializedName("userMobilePhone")
+    private String userMobilePhone;
+
+
+    public JoinData(String userID, String userPwd, String userEmail,
+                    String userName, String userMobilePhone) {
+        this.userID = userID;
         this.userPwd = userPwd;
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.userMobilePhone=userMobilePhone;
     }
 }
