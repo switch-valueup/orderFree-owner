f27559d145fba3cad4882a368be4a8a3b5b2c704
Yeon <jryoun1@naver.com>
2020-06-23 23:14:33 +0900

    서버쪽 코딩할 때 userEmail이 넘어가는데 userID가 넘어가는 것처럼 되어있어서 userEmail로 수정

--- app/src/main/java/com/example/userapplication/login/data/LoginData.java
+++ app/src/main/java/com/example/userapplication/login/data/LoginData.java
@@ -3,14 +3,14 @@ package com.example.userapplication.login.data;
 import com.google.gson.annotations.SerializedName;
 
 public class LoginData {
-    @SerializedName("userID")
-    String userID;
+    @SerializedName("userEmail")
+    String userEmail;
 
     @SerializedName("userPwd")
     String userPwd;
 
-    public LoginData(String userID, String userPwd){
-        this.userID=userID;
+    public LoginData(String userEmail, String userPwd){
+        this.userEmail=userEmail;
         this.userPwd=userPwd;
     }
 }
